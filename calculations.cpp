6293d860a4125be793f31a44ff20ad3a64578310
Yiru-Liu <66910916+Yiru-Liu@users.noreply.github.com>
2022-11-16 08:26:57 -0500

    Actuate both motors simultaneously

--- calculations.cpp
+++ calculations.cpp
@@ -11,30 +11,38 @@
 const int LEG_PART_LENGTH = 6.4375;
 
 class Leg {
+    volatile double hip_angle, knee_angle;
     motor hip_motor, knee_motor;
+    event move_hip_event, move_knee_event;
 
     public:
         Leg(motor hip, motor knee) {
             hip_motor = hip;
             knee_motor = knee;
+            move_hip_event(set_hip);
+            move_knee_event(set_knee);
         }
 
-        void set_hip(double angle) {
-            hip_motor.setPosition(angle, degrees);
+        void set_hip() {
+            hip_motor.setPosition(hip_angle, degrees);
         }
 
-        void set_knee(double angle) {
-            knee_motor.setPosition(angle, degrees);
+        void set_knee() {
+            knee_motor.setPosition(knee_angle, degrees);
+        }
+
+        void update_leg() {
+            move_hip_event.broadcast();
+            move_knee_event.broadcast();
         }
 
         void set_overall_leg(double length, double angle) {
-            double knee_angle = 2 * asin(length / (2 * LEG_PART_LENGTH));
-            double hip_angle = 90 - knee_angle/2;
+            knee_angle = 2 * asin(length / (2 * LEG_PART_LENGTH));
+            hip_angle = 90 - knee_angle/2;
             knee_angle -= 180.0;    // So the angle is 0 when the leg is straight
             knee_angle *= -1.0;     // Knee typically bends backwards
             hip_angle += angle;     // Bend overall leg
-            set_hip(hip_angle);
-            set_knee(knee_angle);
+            update_leg();
         }
 
         void set_foot_position(double x, double y) {
